d5571104fa36569ce56e31b2473cf05fdeafaa4f
Thibaut <lamurethibaut@gmail.com>
2023-09-01 15:40:32 +0100

    starting

---
+++ SystematicStrategies/Portfolio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace SystematicStrategies {
+    internal class Portfolio
+    {
+        Dictionary<PricingLibrary.MarketDataFeed.ShareValue, float> portfolioMap;
+        float previousDelta;
+        float previousFreeRateQuantity;
+        public Portfolio(List<PricingLibrary.MarketDataFeed.ShareValue> shares)
+        {
+            Dictionary<PricingLibrary.MarketDataFeed.ShareValue, float> portfolioMap = new Dictionary<PricingLibrary.MarketDataFeed.ShareValue, float>();
+            if (shares != null)
+            {
+                foreach (PricingLibrary.MarketDataFeed.ShareValue share in shares)
+                {
+                    if (share != null) { portfolioMap.Add(share, 1); }
+                }
+            }
+            this.portfolioMap = portfolioMap;
+        }
+
+        public double updatePortfolioValue(PricingLibrary.Computations.Pricer pricer, int t, List<PricingLibrary.MarketDataFeed.ShareValue> marketData, double prevPortfolioValue) 
+        {
+            DateTime prevDate = marketData[t - 1].DateOfPrice;
+            DateTime currDate = marketData[t].DateOfPrice;
+
+            double[] spots = new double[] { marketData[t-1].Value };
+            double prevDelta = pricer.Price(PricingLibrary.TimeHandler.MathDateConverter.ConvertToMathDistance(prevDate, marketData.Last().DateOfPrice), spots).Deltas[0];
+            double currPrice = marketData[t].Value;
+            double prevPrice = marketData[t - 1].Value;
+            double freeRate = PricingLibrary.MarketDataFeed.RiskFreeRateProvider.GetRiskFreeRateAccruedValue(prevDate, currDate);
+            return prevDelta * currPrice + freeRate * (prevPortfolioValue - prevDelta * prevPrice);
+        }
+    }
+}
--- SystematicStrategies/Program.cs
+++ SystematicStrategies/Program.cs
@@ -1,5 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var test = PricingLibrary.Utilities.SampleMarketData.Sample();
-Console.WriteLine("Hello, World!");
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
+namespace SystematicStrategies
+{
+    class Program
+    {
+        static void Main()
+        {
+            var marketData = PricingLibrary.Utilities.SampleMarketData.Sample();
+            var testParameters = PricingLibrary.Utilities.SampleTestParameters.Sample();
+            List<PricingLibrary.MarketDataFeed.ShareValue> sharesValues = marketData.ToList();
+
+            PricingLibrary.Computations.Pricer pricer = new PricingLibrary.Computations.Pricer(testParameters);
+            var p = pricer.Price(PricingLibrary.TimeHandler.MathDateConverter.ConvertToMathDistance(sharesValues[0].DateOfPrice, sharesValues.Last().DateOfPrice), new double[]{ sharesValues[0].Value }).Price;
+            List<double> portfolioValues = new List<double>() { p};
+
+            List<PricingLibrary.MarketDataFeed.ShareValue> shares = new List<PricingLibrary.MarketDataFeed.ShareValue>();
+            shares.Add(marketData.FirstOrDefault());
+            Portfolio portfolio = new Portfolio(shares);
+
+            var vt1 = portfolio.updatePortfolioValue(pricer, 1, sharesValues, p);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
